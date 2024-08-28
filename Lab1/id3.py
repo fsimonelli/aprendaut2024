ec08066a35f69baed06387b0f5a9c212b1fe31f3
xiribarnegaray <xavieriribarnegaray@gmail.com>
2024-08-28 18:45:51 -0300

    feat: end id3 implementation

--- Lab1/id3.py
+++ Lab1/id3.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+import pprint as pprint
 
-DATASET_FILE = "./formatted_weather_table.csv"
+TESTING_DATASET_FILE = "./formatted_weather_table.csv"
+PEDRO_DATASET_FILE = "./pedro.csv"
+DATASET_FILE = "./lab1_dataset.csv"
 
-dataset = pd.read_csv(DATASET_FILE, sep=",")
-
-# implementar el algoritmo ID3 con la extensión para atributos numéricos
+pedro_dataset = pd.read_csv(PEDRO_DATASET_FILE, sep=",")
+testing_dataset = pd.read_csv(TESTING_DATASET_FILE, sep=",")
 
 class Node(object):
     def __init__(self, data):
@@ -21,11 +23,13 @@ class Node(object):
 # Entropy for boolean functions.
 def entropy(dataset, target):
     values = dataset[target].value_counts()
-    total = values.sum()
+    total = dataset.shape[0]
+    p0 = values.iloc[0]/total
     if (len(values) > 1):
-        return -(values.iloc[0]/total)*np.log2(values.iloc[0]/total) - (values.iloc[1]/total) * np.log2(values.iloc[1]/total)
+        p1 = values.iloc[1]/total
+        return -(p0)*np.log2(p0) - (p1) * np.log2(p1)
     else: 
-        return -(values.iloc[0]/total)*np.log2(values.iloc[0]/total)
+        return -(p0)*np.log2(p0)
 
 def best_attribute(dataset, target, attributes):
     entropies = []
@@ -34,43 +38,38 @@ def best_attribute(dataset, target, attributes):
         for value, count in dataset[attribute].value_counts().items():
             res += count*entropy(dataset.loc[dataset[attribute] == value], target)
         entropies.append(res / dataset.shape[0])
-    return entropies.index(min(entropies))
+    return attributes[entropies.index(min(entropies))]
 
 def id3(dataset, target, attributes):
-    print(dataset)
-    if len(attributes) == 0:
-        return Node(dataset[target].value_counts().index.tolist()[0])
-    #print(dataset[target].value_counts().index.tolist())
-    if len(dataset[target].value_counts().index.tolist()) == 1:
-        return Node(dataset[target].value_counts().index.tolist()[0])
+    if len(attributes) == 0 or len(dataset[target].value_counts().index) == 1:
+        return dataset[target].iloc[0]
     else :
         best = best_attribute(dataset, target, attributes)
-        print(best_attribute(dataset, target, attributes))
-        tree = Node(attributes[best])
-        for value in dataset[attributes[best]].value_counts().index.tolist():
-            #print(dataset[atributos[best]].value_counts())
-            new_attributes = attributes.copy()
-            new_attributes.remove(attributes[best])
-            #print(attributes)
-            #print(new_attributes)
-            #print(dataset.iloc[(dataset[atributos[best]] == value).values])
-            tree.add_child(id3((dataset.iloc[(dataset[attributes[best]] == value).values]).drop(columns=attributes[best]), target, new_attributes))
+        tree = {best: {}}
+        new_attributes = attributes.copy()
+        new_attributes.remove(best)
+        
+        for value in dataset[best].value_counts().index:
+            tree[best][value] = id3(dataset.loc[dataset[best] == value], target, new_attributes)
         return tree
-    
-def printTree(node, i):
-    for j in range(0,i):
-        print("-", end = " ")
-    if (not node.isLeaf()):
-        print(node.data)
-        for child in node.children:
-            printTree(child, i+1)
-    else:
-        print(node.data)
 
-#atributos = ['Dedicación','Dificultad','Horario','Humedad','Humor Doc']  
-#print('El mejor atributo es: ' + atributos[best_attribute(dataset, "Salva", atributos)])
-atributos = ['Outlook','Temp.','Humidity','Wind']
-#print(dataset)
-#resTree = id3(dataset, "Salva", atributos)
-resTree = id3(dataset, "Decision", atributos)
-printTree(resTree, 0)
+
+pedro_attributes = ["Dedicación", "Dificultad", "Horario", "Humedad", "Humor Doc"]
+pedro_target = "Salva"
+
+testing_attributes = ["Outlook", "Temp.", "Humidity", "Wind"]
+testing_target = "Decision"
+
+pprint.pprint(id3(pedro_dataset, pedro_target, pedro_attributes))
+pprint.pprint(id3(testing_dataset, testing_target, testing_attributes))
+
+# Used Panda Functions
+
+# read_csv
+# value_counts => https://pandas.pydata.org/docs/reference/api/pandas.Series.value_counts.html
+# items => https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.items.html
+# loc => https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.loc.html
+# iloc => https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.iloc.html
+# drop => https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.drop.html
+# shape => https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.shape.html
+# index => https://pandas.pydata.org/docs/reference/api/pandas.Index.html
