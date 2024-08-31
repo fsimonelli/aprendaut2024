936d8fd3501fe3f9a06f5431ee49f0191966a6b0
fsimonelli <franciscosimonelli6@gmail.com>
2024-08-31 12:20:48 -0300

    refactor:names y pruebas scikit learn

--- Lab1/id3.py
+++ Lab1/id3.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import numpy as np
 import pprint as pprint
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn import tree
+import matplotlib.pyplot as plt
 
 TESTING_DATASET_FILE = "./formatted_weather_table.csv"
 PEDRO_DATASET_FILE = "./pedro.csv"
 CONTINUOUS_DATASET_FILE = "./solvency_table.csv"
 DATASET_FILE = "./lab1_dataset.csv"
-# Continuous attributes: time, age, wtkg, karnof, preanti, cd40, cd420, cd80, cd820
+# Continuous features: time, age, wtkg, karnof, preanti, cd40, cd420, cd80, cd820
 
 pedro_dataset = pd.read_csv(PEDRO_DATASET_FILE, sep=",")
 testing_dataset = pd.read_csv(TESTING_DATASET_FILE, sep=",")
@@ -23,9 +26,9 @@ def generate_every_pair_from_list(list, max_range_splits):
     return res
             
 #O(n^i)
-def get_splits(dataset, attribute, target, max_range_splits):
+def get_splits(dataset, feature, target, max_range_splits):
     min_entropy = 1
-    dataset = dataset.sort_values(by=attribute)
+    dataset = dataset.sort_values(by=feature)
     current_target = dataset[target].iloc[0]
     dataset_size = dataset.shape[0]
     candidate_splits = []
@@ -34,16 +37,16 @@ def get_splits(dataset, attribute, target, max_range_splits):
     # Finding splits
     for i in range(1,dataset_size):
         if current_target != dataset[target].iloc[i]:
-            candidate_splits.append((dataset[attribute].iloc[i-1] + dataset[attribute].iloc[i])/2)
+            candidate_splits.append((dataset[feature].iloc[i-1] + dataset[feature].iloc[i])/2)
             current_target = dataset[target].iloc[i]
             
     splits = generate_every_pair_from_list(candidate_splits, max_range_splits)
     
     for split in splits:
-        split_dataset = actually_split(dataset.copy(), attribute, split)
+        split_dataset = actually_split(dataset.copy(), feature, split)
         aux_entropy = 0
-        for value, count in split_dataset[attribute].value_counts().items():
-            aux_entropy += count*entropy(split_dataset.loc[split_dataset[attribute] == value], target)
+        for value, count in split_dataset[feature].value_counts().items():
+            aux_entropy += count*entropy(split_dataset.loc[split_dataset[feature] == value], target)
         aux_entropy = aux_entropy / split_dataset.shape[0]
             
         if (aux_entropy < min_entropy):
@@ -52,9 +55,7 @@ def get_splits(dataset, attribute, target, max_range_splits):
             
     return (min_entropy,best_values)
 
-
-
-def actually_split(dataset, attribute, split):
+def actually_split(dataset, feature, split):
     def discretize(x):
         if (len(split) == 2):
             if (x < split[1] and x >= split[0]):
@@ -62,7 +63,7 @@ def actually_split(dataset, attribute, split):
         if(x < split[0]):
             return '<' + str(split[0])
         return '>' + str(split[-1])
-    dataset[attribute] = dataset[attribute].apply(discretize)
+    dataset[feature] = dataset[feature].apply(discretize)
     return dataset
 
 # Entropy for boolean functions.
@@ -76,55 +77,74 @@ def entropy(dataset, target):
     else: 
         return -(p0)*np.log2(p0)
 
-def best_attribute(dataset, target, attributes, max_range_splits):
+def best_feature(dataset, target, features, max_range_splits):
     entropies = []
     continuous = {}
-    for attribute in attributes:
-        # Continuous-Valued attribute
-        if attribute in continuous_attributes:
-            aux_entropy, best_split = get_splits(dataset, attribute, target, max_range_splits)
+    for feature in features:
+        # Continuous-Valued feature 
+        if feature in continuous_features:
+            aux_entropy, best_split = get_splits(dataset, feature, target, max_range_splits)
             entropies.append(aux_entropy)
-            continuous[attribute] = best_split
+            continuous[feature] = best_split
         else :
             res = 0
-            for value, count in dataset[attribute].value_counts().items():
-                res += count*entropy(dataset.loc[dataset[attribute] == value], target)
+            for value, count in dataset[feature].value_counts().items():
+                res += count*entropy(dataset.loc[dataset[feature] == value], target)
             entropies.append(res / dataset.shape[0])
-            continuous[attribute] = None
+            continuous[feature] = None
             
-    best_attribute = attributes[entropies.index(min(entropies))]
+    best_feature = features[entropies.index(min(entropies))]
     
-    if (continuous[best_attribute] is None):
-        return best_attribute, dataset
-    return best_attribute, actually_split(dataset.copy(), best_attribute, continuous[best_attribute])
+    if (continuous[best_feature] is None):
+        return best_feature, dataset
+    return best_feature, actually_split(dataset.copy(), best_feature, continuous[best_feature])
 
-def id3(dataset, target, attributes, max_range_splits):
-    if len(attributes) == 0 or len(dataset[target].value_counts().index) == 1:
-        return dataset[target].value_counts().index[0]
-    else :
-        best, dataset = best_attribute(dataset, target, attributes, max_range_splits)
-        tree = {best: {}}
-        new_attributes = attributes.copy()
-        new_attributes.remove(best)
+def id3(dataset, target, features, max_range_splits):
+    if len(features) == 0 or len(dataset[target].value_counts().index) == 1:
+        # value_counts[0] is either the only or the most common target value left in the current dataset.
+        return dataset[target].value_counts().index[0] 
+    
+    best, dataset = best_feature(dataset, target, features, max_range_splits)
+    decision_tree = {best: {}}
         
-        for value in dataset[best].value_counts().index:
-            tree[best][value] = id3(dataset.loc[dataset[best] == value], target, new_attributes, max_range_splits)
-        return tree
+    new_features = features.copy()
+    new_features.remove(best)
+        
+    for value in dataset[best].value_counts().index:
+        examples = dataset.loc[dataset[best] == value]
+        if (len(examples) == 0):
+            decision_tree[best][value] = dataset.value_counts().index[0]
+        else:
+            decision_tree[best][value] = id3(examples, target, new_features, max_range_splits)
+    return  decision_tree
 
-pedro_attributes = ["Dedicación", "Dificultad", "Horario", "Humedad", "Humor Doc"]
+
+
+
+
+pedro_features = ["Dedicación", "Dificultad", "Horario", "Humedad", "Humor Doc"]
 pedro_target = "Salva"
 
-testing_attributes = ["Outlook", "Temp.", "Humidity", "Wind"]
+testing_features = ["Outlook", "Temp.", "Humidity", "Wind"]
 testing_target = "Decision"
 
-continuous_attributes = ['EBIT_over_A','ln_A_over_L','RE_over_A','FCF_over_A']
+continuous_features = ['EBIT_over_A','ln_A_over_L','RE_over_A','FCF_over_A']
 continuous_target = 'Solvency'
 
-#pprint.pprint(id3(pedro_dataset, pedro_target, pedro_attributes))
+pprint.pprint(id3(pedro_dataset, pedro_target, pedro_features, 2))
 
-#pprint.pprint(id3(testing_dataset, testing_target, testing_attributes))
+pprint.pprint(id3(testing_dataset, testing_target, testing_features, 2))
 
-pprint.pprint(id3(continuous_dataset, continuous_target, continuous_attributes, 3))
+pprint.pprint(id3(continuous_dataset, continuous_target, continuous_features, 2))
+
+X = continuous_dataset.drop('Solvency', axis=1) 
+y = continuous_dataset['Solvency']
+
+clf = tree.DecisionTreeClassifier(criterion='entropy')
+clf = clf.fit(X, y)
+
+tree.plot_tree(clf)
+
 
 # Used Panda Functions
 
