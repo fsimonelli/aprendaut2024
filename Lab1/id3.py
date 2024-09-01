425ed53cbc5dca65fd2f0d3e85d308fb828e02fa
fsimonelli <franciscosimonelli6@gmail.com>
2024-09-01 00:13:53 -0300

    feat: max_range_split capaz

--- Lab1/id3.py
+++ Lab1/id3.py
@@ -4,6 +4,10 @@ import pprint as pprint
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn import tree
 import matplotlib.pyplot as plt
+from datetime import datetime
+import json
+
+startTime = datetime.now()
 
 TESTING_DATASET_FILE = "./formatted_weather_table.csv"
 PEDRO_DATASET_FILE = "./pedro.csv"
@@ -28,22 +32,21 @@ def generate_every_pair_from_list(list, max_range_splits):
             
 #O(n^i)
 def get_splits(dataset, feature, target, max_range_splits):
-    min_entropy = 1
+    min_entropy = 2
     dataset = dataset.sort_values(by=feature)
-    print(dataset[target])
     current_target = dataset[target].iloc[0]
     dataset_size = dataset.shape[0]
     candidate_splits = []
     best_values = []
     
     # Finding splits
-    for i in range(1,dataset_size):
+    for i in range(1, dataset_size):
         if current_target != dataset[target].iloc[i]:
             candidate_splits.append((dataset[feature].iloc[i-1] + dataset[feature].iloc[i])/2)
             current_target = dataset[target].iloc[i]
-            
+
     splits = generate_every_pair_from_list(candidate_splits, max_range_splits)
-    
+ 
     for split in splits:
         split_dataset = actually_split(dataset.copy(), feature, split)
         aux_entropy = 0
@@ -58,14 +61,10 @@ def get_splits(dataset, feature, target, max_range_splits):
     return (min_entropy,best_values)
 
 def actually_split(dataset, feature, split):
-    print(split)
-    def discretize(x):
-        if (len(split) == 2):
-            if (x < split[1] and x >= split[0]):
-                return '[' + str(split[0]) + ',' + str(split[1]) + ']' 
-        if(x < split[0]):
-            return '<' + str(split[0])
-        return '>' + str(split[-1])
+    discretize = lambda x: ('[' + str(split[0]) + ',' + str(split[1]) + ']' if len(split) == 2 and split[0] <= x < split[1] else
+                            '<' + str(split[0]) if x < split[0] else
+                            '>' + str(split[-1])
+                        )
     dataset[feature] = dataset[feature].apply(discretize)
     return dataset
 
@@ -94,11 +93,11 @@ def best_feature(dataset, target, features, max_range_splits):
             for value, count in dataset[feature].value_counts().items():
                 res += count*entropy(dataset.loc[dataset[feature] == value], target)
             entropies.append(res / dataset.shape[0])
-            continuous[feature] = None
+            # continuous[feature] = None
             
     best_feature = features[entropies.index(min(entropies))]
     
-    if (continuous[best_feature] is None):
+    if not (best_feature in continuous):
         return best_feature, dataset
     return best_feature, actually_split(dataset.copy(), best_feature, continuous[best_feature])
 
@@ -106,20 +105,90 @@ def id3(dataset, target, features, max_range_splits):
     if len(features) == 0 or len(dataset[target].value_counts().index) == 1:
         # value_counts[0] is either the only or the most common target value left in the current dataset.
         return dataset[target].value_counts().index[0] 
-    
     best, dataset = best_feature(dataset, target, features, max_range_splits)
     decision_tree = {best: {}}
-        
     new_features = features.copy()
     new_features.remove(best)
-        
     for value in dataset[best].value_counts().index:
         examples = dataset.loc[dataset[best] == value]
+        # print(examples)
         if (len(examples) == 0):
             decision_tree[best][value] = dataset.value_counts().index[0]
         else:
             decision_tree[best][value] = id3(examples, target, new_features, max_range_splits)
+        # pprint.pprint(decision_tree)
     return  decision_tree
+
+
+""" def fun(tree, dataset):
+    
+    d -- decision tree dictionary
+    t -- testing examples in form of pandas dataset
+    
+    res = []
+    for _, e in dataset.iterrows():
+        res.append(predict(tree, e))
+    return res
+
+def predict(d, e):
+    
+    d -- decision tree dictionary
+    e -- a testing example in form of pandas series
+    
+    current_node = list(d.keys())[0]
+    current_branch = d[current_node][e[current_node]]
+    # if leaf node value is string then its a decision
+    if isinstance(current_branch, str):
+        return current_branch
+    # else use that node as new searching subtree
+    else:
+        return predict(current_branch, e)
+
+print(fun(tree, dataset)) """
+
+""" def printjson(jsonObj):
+    for key in jsonObj:
+        print(key)
+        if(key is dict):
+            for value in jsonObj[key]:
+                print(value)
+                printjson(value) """
+                
+def iterate_nested_dict(nested_dict): 
+    for key, value in nested_dict.items(): 
+        #if key es un attributo
+        # buscamos columna y nos metemos en value y 
+        # Comparamos con cada key del value. llamamos recursivamente
+        # if key
+        if isinstance(value, dict): 
+            print(f"Key: {key}, Value: {value}") 
+            iterate_nested_dict(value)
+        else: 
+            print(f"Key: {key}, Value: {value}") 
+
+"""
+def iterateThorughTree(tree,attributes,target,row):
+    for key, value in tree:
+        if key is in attributes:
+            res = iterateThroughTree(value,attributes,target,row)
+            if not (res is None):
+                return res
+            
+
+"""
+
+treejson = {'Outlook': {'Overcast': 'Yes',
+             'Rain': {'Wind': {'Strong': 'No', 'Weak': 'Yes'}},
+             'Sunny': {'Humidity': {'High': 'No', 'Normal': 'Yes'}}}}
+
+#iterate_nested_dict(treejson)
+
+""" def verify(dataset, target, features, tree):
+    dataset_size = dataset.shape[0]
+    for i in range(1,dataset_size):
+        
+        #dataset[target].iloc[i] """
+
 
 
 pedro_features = ["Dedicación", "Dificultad", "Horario", "Humedad", "Humor Doc"]
@@ -137,25 +206,26 @@ features = ['time', 'trt', 'age', 'wtkg', 'hemo', 'homo', 'drugs', 'karnof',
        'oprior', 'z30', 'zprior', 'preanti', 'race', 'gender', 'str2', 'strat',
        'symptom', 'treat', 'offtrt', 'cd40', 'cd420', 'cd80', 'cd820']
 
-print(features)
+# id3(dataset, target, features, 2)
 
-pprint.pprint(id3(dataset, target, features, 2))
+pprint.pprint(id3(dataset, target, features, 3))
+print(datetime.now() - startTime)
 
 # pprint.pprint(id3(pedro_dataset, pedro_target, pedro_features, 2))
 
 # pprint.pprint(id3(testing_dataset, testing_target, testing_features, 2))
 
-# pprint.pprint(id3(continuous_dataset, continuous_target, continuous_features, 3))
+# pprint.pprint(id3(continuous_dataset, solvency_continuous_target, solvency_continuous_features, 2))
 
-# X = continuous_dataset.drop('Solvency', axis=1) 
-# y = continuous_dataset['Solvency']
+""" X = dataset.drop('cid', axis=1) 
+y = dataset['cid']
 
-# plt.figure()
-# clf = tree.DecisionTreeClassifier(criterion='entropy')
-# clf = clf.fit(X, y)
+plt.figure()
+clf = tree.DecisionTreeClassifier(criterion='entropy')
+clf = clf.fit(X, y)
 
-# tree.plot_tree(clf, class_names=['0', '1'])
-# plt.show()
+tree.plot_tree(clf, class_names=['0', '1'])
+plt.show() """
 
 
 # Used Panda Functions
